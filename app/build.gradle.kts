ec7faf984759908a5e11c14cc429b2c22d34afb4
FaezBarghasa <faez.barghasa.org@gmail.com>
2026-06-28 17:01:23 +0330

    try to fix gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -123,6 +123,7 @@ dependencies {
   implementation(libs.okhttp)
   // implementation(libs.play.services.location)
   implementation(libs.retrofit)
+  implementation(libs.surrealdb)
   testImplementation(libs.androidx.compose.ui.test.junit4)
   testImplementation(libs.androidx.core)
   testImplementation(libs.androidx.junit)
